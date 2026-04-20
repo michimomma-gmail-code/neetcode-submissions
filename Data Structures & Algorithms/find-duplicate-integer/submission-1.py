79b5b6c3ea01bd05ea5700a06932689146259d57
michimomma-gmail-code <michimomma@gmail.com>
2026-04-20 11:01:44 -0700

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # [1 2 2 3] vs [1 2 3 3] vs [1 2]
+        # 
+        for i, num in enumerate(nums):
+            if nums[abs(num)-1] < 0:
+                return abs(num)
+            nums[abs(num)-1] *= -1
+
+#        print(nums)
+
+        return -1
