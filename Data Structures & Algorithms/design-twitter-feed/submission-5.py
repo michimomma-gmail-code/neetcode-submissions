2882366cade859fc3f8ca801afcfdcb69709eec1
michimomma-gmail-code <michimomma@gmail.com>
2026-04-29 12:37:22 -0700

    Add: design-twitter-feed - submission-5

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-5.py
@@ -0,0 +1,54 @@
+class Twitter:
+
+    def __init__(self):
+    # for each use, keep most recent 10 posts
+    # graph: user A follow (->) user B
+        self.user_post = defaultdict( deque )
+        self.user_to_user = defaultdict(set)
+        self.count = 0
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.user_post[userId].append( (self.count, tweetId) )
+        self.count += 1
+#        print(self.user_post)
+        while len(self.user_post[userId]) > 10:
+            self.user_post[userId].popleft()
+
+    def getNewsFeed0(self, userId: int) -> List[int]:
+        users = self.user_to_user[userId]
+        users.add(userId)
+        maxHeap = []
+
+        for u in users:
+            for idx in range( min(10, len(self.user_post[u])) ):
+                count, tweetId = self.user_post[u][idx]
+                heapq.heappush(maxHeap, (-count, tweetId) )
+        
+        res = []
+        for _ in range( min(10, len(maxHeap)) ):
+            negCount, tweetId = heapq.heappop(maxHeap)
+            res.append( tweetId )
+        return res
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        users = self.user_to_user[userId]
+        users.add(userId)
+        minHeap = []
+
+        for u in users:
+            for idx in range( min(10, len(self.user_post[u])) ):
+                count, tweetId = self.user_post[u][idx]
+                heapq.heappush(minHeap, (count, tweetId) )
+                while len(minHeap) > 10:
+                    heapq.heappop(minHeap)
+        
+        minHeap.sort(reverse = True)
+        return [tweetId for count, tweetId in minHeap]
+
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.user_to_user[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.user_to_user[followerId]:
+            self.user_to_user[followerId].remove(followeeId)
