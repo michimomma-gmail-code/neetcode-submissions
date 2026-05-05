c645ecb21c5c3649d0117babb8e16117e8744835
michimomma-gmail-code <michimomma@gmail.com>
2026-05-04 18:18:50 -0700

    Add: design-word-search-data-structure - submission-10

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-10.py
@@ -0,0 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()
+            cur = cur.children[char]
+        cur.is_end_of_word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(node, idx):
+#            print(f'idx = {idx}, {len(word)}')
+            if idx >= len(word):
+                result = node.is_end_of_word
+                return result
+
+            if word[idx] == ".":
+                for char in node.children:
+                    if dfs(node.children[char], idx + 1):
+                        return True
+                return False
+            else:
+                if word[idx] in node.children:
+                    if dfs(node.children[ word[idx] ], idx + 1):
+                        return True
+                return False
+        
+#        for c in self.root.children:
+#            print(f'child = {c}')
+        return dfs(self.root, 0)
+
