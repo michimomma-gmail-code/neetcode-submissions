229ddc89b7a5ad891c96c9051893673fdb65b9e7
michimomma-gmail-code <michimomma@gmail.com>
2026-05-09 12:35:57 -0700

    Add: course-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-6.py
@@ -0,0 +1,46 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # [course, pre]
+        # order of courses: pre -> course
+
+        #pre2course = {i : [] for i in range(numCourses)}
+#        pre2course = {p: [] for (p, c) in prerequisites}
+        pre2course = defaultdict(list)
+        for c, p in prerequisites:
+            pre2course[p].append(c)
+#        print(pre2course)
+        visiting = set()
+        done = set()
+        res = []
+
+        def dfs(pre):
+            #loop
+            if pre in done:
+                return True
+            if pre in visiting:
+                return False
+            
+            visiting.add(pre)
+            
+            for crs in pre2course[pre]:
+                if not dfs(crs):
+                    return False
+            
+            done.add(pre)
+            res.append(pre)
+#            print(visiting)
+            visiting.remove(pre)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        
+        return res[::-1]
+
+
+
+
+
+
+
