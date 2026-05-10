8cb243490cc2796697876e941dfd1cfec90cedfa
michimomma-gmail-code <michimomma@gmail.com>
2026-05-09 19:59:16 -0700

    Add: valid-tree - submission-13

---
+++ Data Structures & Algorithms/valid-tree/submission-13.py
@@ -0,0 +1,50 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if not edges:
+            return n == 1
+        
+        graph = defaultdict(list)
+        for n1, n2 in edges:
+            graph[n1].append(n2)
+            graph[n2].append(n1)
+
+#        print(graph)
+        visiting = set()
+        visitingEdge = set()
+        done = set()
+        def dfs(node):
+#            print(f'node = {node}')
+
+            if node in done:
+                return True
+
+            if node in visiting:
+#                print(node, visiting)
+                return False
+
+            visiting.add(node)
+
+            for nei in graph[node]:
+#                print(f'nei = {nei}')
+                n1, n2 = min(node, nei), max(node, nei)
+                if (n1, n2) in visitingEdge:
+                    continue
+                else:
+                    visitingEdge.add( (n1, n2))
+                if not dfs(nei):
+                    return False
+
+            visiting.remove(node)
+            done.add(node)
+
+            return True
+
+        # for node in list(graph.keys()):
+        #     if not dfs(node):
+        #         return False
+        #     break
+        #node = edges[0][0]
+        if not dfs(0):
+            return False
+
+        return len(done) == n
