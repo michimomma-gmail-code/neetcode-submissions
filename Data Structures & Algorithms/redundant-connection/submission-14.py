c28724ec2795cbbced4308f80cc4443e8b1fbdaf
michimomma-gmail-code <michimomma@gmail.com>
2026-05-10 15:37:52 -0700

    Add: redundant-connection - submission-14

---
+++ Data Structures & Algorithms/redundant-connection/submission-14.py
@@ -0,0 +1,55 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        adj = [ [] for _ in range(n + 1)]
+        edge_s = {}
+        for i, (u, v) in enumerate(edges):
+            adj[u].append(v)
+            adj[v].append(u)
+            edge_s[(u, v)] = i
+            edge_s[(v, u)] = i
+
+        visiting = set()
+        visited = []
+        self.result = []
+        done = set()
+
+        def dfs(node, parent):
+            if node in done:
+                return True
+
+            if node in visiting:
+                eg = (parent, node)
+                maxid = edge_s[eg]
+#                print(visited, eg)
+#                print(maxid)
+                while visited:
+                    u, v = visited.pop()
+                    if edge_s[(u, v)] > maxid:
+                        maxid = edge_s[(u, v)]
+                    if u == node or v == node:
+                        break
+                self.result = edges[maxid]
+#                print(f'result = {result}')
+                #print(node, visited)
+                return False
+
+            visiting.add(node)
+            visited.append([parent, node])
+
+            for nei in adj[node]:
+                if nei == parent:
+                    continue
+                if not dfs(nei, node):
+                    return False
+
+#            print(f'done?')
+            visiting.remove(node)
+            visited.pop()
+            done.add(node)
+
+            return True
+
+        dfs(n, None)
+
+        return self.result
