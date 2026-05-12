6a4ae13786a18ea0c1be4d78d8374e4e1e6a0375
michimomma-gmail-code <michimomma@gmail.com>
2026-05-12 13:13:10 -0700

    Add: swim-in-rising-water - submission-1

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+
+        n = len(grid)
+        cell2dist =  [[ -float("inf") for _ in range(n) ] for _ in range(n) ]
+        cell2dist[0][0] = grid[0][0]
+
+        minheap = [ (grid[0][0], 0, 0) ] # (dist, x, y)
+        direction = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        done = set()
+
+        while minheap:
+
+            dist, x, y = heapq.heappop(minheap)
+
+            if x == n - 1 and y == n - 1:
+                return dist
+
+            for dx, dy in direction:
+                nx, ny = x + dx, y + dy
+                if not (0 <= nx < n and 0 <= ny < n) or (nx, ny) in done:
+                    continue
+                done.add((nx, ny))
+                ndist = grid[nx][ny]
+                cell2dist[nx][ny] = max(cell2dist[x][y], ndist)
+                heapq.heappush(minheap, (cell2dist[nx][ny], nx, ny))
+
+
+        return 0
