511799c1afb151627c9f89afea3481694664442f
michimomma-gmail-code <michimomma@gmail.com>
2026-05-14 11:43:26 -0700

    Add: house-robber-ii - submission-6

---
+++ Data Structures & Algorithms/house-robber-ii/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        # A ignore last house (nums[n])
+        # B ignore first house (nums[0])
+        # A
+
+        if n == 1:
+            return nums[0]
+
+        dp0 = dp1 = dp2 = 0
+        for i in range(n-1):
+            dp2 = max(dp1, dp0 + nums[i])
+            dp1, dp0 = dp2, dp1
+
+        dpB0 = dpB1 = dpB2 = 0
+        for i in range(1, n):
+            dpB2 = max(dpB1, dpB0 + nums[i])
+            dpB1, dpB0 = dpB2, dpB1
+
+        res = max(dp2, dpB2)
+        return res
