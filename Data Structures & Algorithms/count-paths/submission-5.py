5791b3842c7d52995e535ca0742f692601601be5
michimomma-gmail-code <michimomma@gmail.com>
2026-05-16 12:29:07 -0700

    Add: count-paths - submission-5

---
+++ Data Structures & Algorithms/count-paths/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def uniquePaths0(self, m: int, n: int) -> int:
+        dp = [ [1] * n for _ in range(m)]
+#        dp[0][0] = 1
+
+        for r in range(1, m):
+            for c in range(1, n):
+                dp[r][c] = max(dp[r][c], dp[r - 1][c]+ dp[r][c - 1])
+
+        return dp[m - 1][n - 1]
+
+    def uniquePaths(self, m: int, n: int) -> int:
+#        dp = [ [1] * n for _ in range(m)]
+#        dp[0][0] = 1
+        dp2 = [1] * n
+
+        for r in range(1, m):
+            for c in range(1, n):
+#                dp[r][c] = max(dp[r][c], dp[r - 1][c]+ dp[r][c - 1])
+#                dp2[c] = max(dp2[c], dp2[c] + dp2[c - 1])
+                dp2[c] = max(dp2[c], dp2[c] + dp2[c - 1])
+        return dp2[n - 1]
