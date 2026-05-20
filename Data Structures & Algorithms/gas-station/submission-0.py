56dad749350b39692ce42c5a23dac32b860e3091
michimomma-gmail-code <michimomma@gmail.com>
2026-05-20 14:54:27 -0700

    Add: gas-station - submission-0

---
+++ Data Structures & Algorithms/gas-station/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+# Input: gas = [1,2,3,4], cost = [2,2,4,1]
+# st = 2
+# index = (2 + 0) % 4 = 2
+# q = gas[2] + 0 = 3 
+# cost[2 + 1] = 
+#
+
+        n = len(gas)
+
+        def do(st):
+            i = st
+            count = 0
+            resi = 0
+            while count < n:
+                index = (i + count) % n
+                quantity = gas[index] + resi
+                if quantity < cost[ index % n]:
+                    return False
+                else:
+                    resi = quantity - cost[ index % n]
+                    count += 1
+
+            return True
+
+
+        for i in range(n):
+            if do(i):
+                return i
+
+        return -1            
