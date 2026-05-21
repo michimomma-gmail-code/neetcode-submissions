24dd657b23217b5bdd82da4e1d85b9d1a171bb12
michimomma-gmail-code <michimomma@gmail.com>
2026-05-21 09:59:11 -0700

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.py
@@ -0,0 +1,72 @@
+class Solution:
+    def partitionLabels0(self, s: str) -> List[int]:
+        
+        letterCount = Counter(list(s))
+
+#        print(letterCount)
+
+        n = len(s)
+
+        res = []
+        currentSet = {s[0]}
+        letterCount[s[0]] -= 1
+        currentStart = 0
+        for i in range(1, len(s)):
+            # for substring so far s[0:i]
+            # if count of all letters are 0, split (put in the res)
+            # if not, decrement count for s[i]
+            #
+            sub_finish = True
+            for char in currentSet:
+                if letterCount[char] > 0:
+                    sub_finish = False
+                    break
+            if sub_finish:
+                res.append(i - currentStart)
+                currentStart = i
+                currentSet = set()
+            letterCount[s[i]] -= 1
+            currentSet.add(s[i])
+        
+        if currentStart <= n - 1:
+            res.append(n - currentStart)
+
+        print(letterCount)
+
+        return res
+
+    def partitionLabels(self, s: str) -> List[int]:
+        
+        letterCount = Counter(list(s))
+
+#        print(letterCount)
+
+        n = len(s)
+
+        res = []
+        currentSet = set()
+        currentStart = 0
+        for i in range(0, len(s)):
+            # for substring so far s[0:i]
+            # if count of all letters are 0, split (put in the res)
+            # if not, decrement count for s[i]
+            #
+            letterCount[s[i]] -= 1
+            currentSet.add(s[i])
+
+            sub_finish = True
+            for char in currentSet:
+                if letterCount[char] > 0:
+                    sub_finish = False
+                    break
+            if sub_finish:
+                res.append(i - currentStart + 1)
+                currentStart = i + 1
+                currentSet = set()
+        
+        if currentStart <= n - 1:
+            res.append(n - currentStart)
+
+        print(letterCount)
+
+        return res
