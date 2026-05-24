f4ca122b0661d375c61b749e962fe35b832ca7fe
michimomma-gmail-code <michimomma@gmail.com>
2026-05-24 04:35:46 -0700

    Add: set-zeroes-in-matrix - submission-8

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-8.py
@@ -0,0 +1,53 @@
+class Solution:
+    def setZeroes0(self, matrix: List[List[int]]) -> None:
+        m, n = len(matrix), len(matrix[0])
+
+        zerorow = [False] * m
+        zerocol = [False] * n
+
+        for r in range(m):
+            for c  in range(n):
+                if matrix[r][c] == 0:
+                    zerorow[r] = zerocol[c] = True
+
+        for r in range(m):
+            if zerorow[r]:
+                matrix[r] = [0] * n
+        
+        for c in range(n):
+            if zerocol[c]:
+                for r in range(m):
+                    matrix[r][c] = 0
+
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        m, n = len(matrix), len(matrix[0])
+
+        is_col0_zero = False
+        is_row0_zero = False
+
+        for r in range(m):
+            for c in range(n):
+                if matrix[r][c] == 0:
+                    matrix[r][0] = matrix[0][c] = 0
+                    if c == 0:
+                        is_col0_zero = True
+                    if r == 0:
+                        is_row0_zero = True
+
+
+        for r in range(1, m):
+            if matrix[r][0] == 0:
+                for c in range(1, n):
+                    matrix[r][c] = 0
+        
+        for c in range(1, n):
+            if matrix[0][c] == 0:
+                for r in range(1, m):
+                    matrix[r][c] = 0
+
+        if is_col0_zero:
+            for r in range(m):
+                matrix[r][0] = 0
+
+        if is_row0_zero:
+            matrix[0] = [0] * n
