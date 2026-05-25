6a914260866d6528f37f1e1bd4451b73274c9333
michimomma-gmail-code <michimomma@gmail.com>
2026-05-25 09:16:56 -0700

    Add: sum-of-two-integers - submission-5

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        # 100 + 111 = 1011
+        # xor: 011
+        # or:  111
+        # and: 100 << 1
+        # 011 + 1000 = 1011
+        # 
+        # 01 + 01 = 10
+        # xor = 00
+        # and = 01 << 10
+        # 10 | 00 = 10
+
+#        print(bin(a), bin(b))
+        mask = (1 << 32) - 1
+#        print(bin(mask))
+        res = a & mask
+        carry = b & mask
+        while carry != 0:
+
+            carry, res = (res & carry) << 1, res ^ carry
+            carry &= mask
+            res &= mask
+#            b = carry
+        max_int = 0x7FFFFFFF
+        if res <= max_int:
+            return res
+        else:
+            return ~(res ^ mask)
