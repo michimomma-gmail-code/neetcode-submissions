2dc6d1f283669c16c324b16df4686cbeeef50f59
michimomma-gmail-code <michimomma@gmail.com>
2026-05-31 06:56:45 -0700

    Add: boats-to-save-people - submission-14

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-14.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        # 
+        people.sort()
+        print(people)
+        count = 0 
+
+        target = limit
+
+        left, right = 0, len(people) - 1
+        # [1, 1, 1, 1, 2, 2, 3, 3], limit = 3
+
+        while left <= right:
+            
+            current = people[left] + people[right]
+            if current <= target:
+                left += 1
+            
+            right -= 1
+            count += 1
+
+#        print(index_pairs)
+        return count
+            
