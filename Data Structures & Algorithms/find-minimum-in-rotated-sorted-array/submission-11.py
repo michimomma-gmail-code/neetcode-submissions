96454933325b61c2686823238f8fa0ca51dcbe40
michimomma-gmail-code <michimomma@gmail.com>
2026-06-03 21:26:11 -0700

    Add: find-minimum-in-rotated-sorted-array - submission-11

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-11.py
@@ -0,0 +1,45 @@
+class Solution:
+    # [<3>,4,5,6,<1>,2]
+    # [<1>,2,3,4,5,6]
+    # answer is either left of each segment (there is eihter one or two segments)
+    # for two segment, can identify by nums[left] > nums[right]
+    # for one segment, nums[0] < nums[-1]
+    def findMin0(self, nums: List[int]) -> int:
+        l, u = 0, len(nums) - 1
+        res = nums[0]
+        while l <= u:
+#            mid = l - ( (l - u) // 2)
+            mid = l + (u - l) // 2
+
+            print(f'l = {l}, mid = {mid}, u = {u}, res = {res}')
+            res = min(res, nums[mid])
+
+            if nums[l] < nums[u]:
+                return min(res, nums[l])
+            else:
+#                if mid == u:
+#                    return nums[u]
+                if nums[mid] >= nums[u]:
+                    l = mid + 1
+                else:
+                    u = mid - 1
+
+        return res
+    def findMin(self, nums: List[int]) -> int:
+        l, u = 0, len(nums) - 1
+        res = nums[0]
+        while l <= u:
+            mid = l + (u - l) // 2
+
+            print(f'l = {l}, mid = {mid}, u = {u}, res = {res}')
+            res = min(res, nums[mid])
+
+            if nums[l] < nums[u]:
+                return min(res, nums[l])
+            else:
+                if nums[mid] >= nums[u]:
+                    l = mid + 1
+                else:
+                    u = mid 
+
+        return res
