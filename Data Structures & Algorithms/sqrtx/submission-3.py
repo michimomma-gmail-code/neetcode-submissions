b4afe74460dc9353cac9171068d02757fc9e169e
michimomma-gmail-code <michimomma@gmail.com>
2026-06-03 18:27:01 -0700

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # y = sqrt(x)
+        # y * y <= x
+
+        # y(left, right)
+        left, right = 0, x
+        while left < right:
+            mid = math.ceil((left + right) / 2 )
+            y2 = mid * mid
+            if y2 == x:
+                return mid
+            elif y2 < x:
+                left = mid 
+            else:
+                right = mid - 1
+
+        return left
