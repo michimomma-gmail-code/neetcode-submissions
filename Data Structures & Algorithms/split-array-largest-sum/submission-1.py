efccd45851663337236a34ede332d6fd9af49436
michimomma-gmail-code <michimomma@gmail.com>
2026-06-04 10:40:48 -0700

    Add: split-array-largest-sum - submission-1

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        
+        def isMaxSumValid(maxSum):
+            # can we split the nums while keeping sum of segment <= given maxSum
+            # the number of segment should be <= k
+
+            cur = 0
+            numSplit = 1
+            for num in nums:
+                if cur + num > maxSum:
+                    numSplit += 1
+                    cur = 0
+                cur += num
+
+            return numSplit <= k
+
+        
+        left, right = max(nums), sum(nums)
+        # larger value is feasible
+        # False, False, True, True
+
+        while left < right:
+            mid = left + (right - left) // 2
+            res = isMaxSumValid(mid)
+#            print(mid, res)
+            if res:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
