ed61075d238bcd3c24f9494d1d82afd52f0a867d
michimomma-gmail-code <michimomma@gmail.com>
2026-06-08 17:54:52 -0700

    Add: insert-into-a-binary-search-tree - submission-4

---
+++ Data Structures & Algorithms/insert-into-a-binary-search-tree/submission-4.py
@@ -0,0 +1,64 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST0(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # 
+        if not root:
+            return TreeNode(val)
+
+        def dfs(node, parent):
+            if not node:
+                if val < parent.val:
+                    parent.left = TreeNode(val)
+                else:
+                    parent.right = TreeNode(val)
+                return
+
+            if val < node.val:
+                dfs(node.left, node)
+            else:
+                dfs(node.right, node)
+
+        dfs(root, None)
+
+        return root
+
+    def insertIntoBST1(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # 
+        if not root:
+            return TreeNode(val)
+
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+
+        return root
+
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+        if not root:
+            return TreeNode(val)
+
+        cur = root
+
+        while cur:
+            if val < cur.val:
+                if not cur.left:
+                    cur.left = TreeNode(val)
+                    return root
+                cur = cur.left
+            else:
+                if not cur.right:
+                    cur.right = TreeNode(val)
+                    return root
+                cur = cur.right
+
+        return root
+            
+
+            
