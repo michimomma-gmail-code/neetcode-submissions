1753d76c0c54c1b42e7d67a4401efee229596671
michimomma-gmail-code <michimomma@gmail.com>
2026-06-13 07:48:20 -0700

    Add: sum-of-all-subset-xor-totals - submission-5

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-5.py
@@ -0,0 +1,53 @@
+class Solution:
+    def subsetXORSum0(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+
+        def dfs(start, current_xor):
+            if start == n:
+                return current_xor
+
+            pick = dfs(start + 1, current_xor ^ nums[start])
+            skip = dfs(start + 1, current_xor)
+
+            return pick + skip
+
+        return dfs(0, 0)
+
+    def subsetXORSum(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+
+        def dfs(start, current_xor):
+            total = current_xor
+            for i in range(start, n):
+                current_xor ^= nums[i]
+                total += dfs(i + 1, current_xor)
+                current_xor ^= nums[i]
+
+            return total
+
+        return dfs(0, 0)
+
+
+    def subsetXORSum1(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        res = []
+        current_xor2 = 0
+        def dfs(start):
+            nonlocal current_xor2
+
+            if start == n:
+                res.append(current_xor2)
+                return
+
+            current_xor2 ^= nums[start]
+            dfs(start + 1)
+
+            current_xor2 ^= nums[start]
+            dfs(start + 1)
+            return
+
+        dfs(0)
+        return sum(res)
