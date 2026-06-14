e88ecb9694fbcaf0b1b1de8d6c3072d618f66c24
michimomma-gmail-code <michimomma@gmail.com>
2026-06-14 09:05:48 -0700

    Add: find-the-town-judge - submission-4

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        # node1 -> node2
+        #       -> node3
+        in_deg = {}
+        out_deg = {}
+        for n1, n2 in trust:
+            in_deg[n2] = 1 + in_deg.get(n2, 0)
+            out_deg[n1] = 1 + out_deg.get(n1, 0)
+
+        for i in in_deg:
+            if i in out_deg:
+                continue
+            if i in in_deg and in_deg[i] == (n - 1):
+                return i
+
+        return -1
