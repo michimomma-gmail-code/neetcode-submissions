adf18e76a6f1a11a34757e53ed10758a1923668d
michimomma-gmail-code <michimomma@gmail.com>
2026-06-21 15:33:46 -0700

    Add: matchsticks-to-square - submission-9

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-9.py
@@ -0,0 +1,40 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        n = len(matchsticks)
+        total = sum(matchsticks)
+        print(total % 4)
+        if total == 0 or total % 4 > 0:
+            return False
+
+        target = total // 4
+        print(f'target = {target}')
+
+        for m in matchsticks:
+            if m > target:
+                return False
+
+        slot = [0] * 4
+
+        def dfs(i):
+            if i == n:
+                for sl in slot:
+                    if sl != target:
+                        return False
+                print('slot = ', slot)
+                return True
+
+            for j in range(4):
+                if slot[j] + matchsticks[i] > target:
+                    continue
+                slot[j] += matchsticks[i]
+                if dfs(i + 1):
+                    return True
+                slot[j] -= matchsticks[i]
+
+            return False
+
+
+        res = dfs(0)
+#        print(slot)
+        return res
+        
