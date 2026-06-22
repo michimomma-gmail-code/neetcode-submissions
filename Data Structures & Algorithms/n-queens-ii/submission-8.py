202257f858b643180e7b4d41ab774548f59879c9
michimomma-gmail-code <michimomma@gmail.com>
2026-06-22 10:47:43 -0700

    Add: n-queens-ii - submission-8

---
+++ Data Structures & Algorithms/n-queens-ii/submission-8.py
@@ -0,0 +1,34 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        
+        visited = set()
+        cols, row_col, row_m_col = set(), set(), set()
+
+        res = []
+        def dfs(r, c):
+            if r == n:
+                res.append( (r, c) )
+                return True
+
+            for c in range(n):
+
+                if (c not in cols and (r+c) not in row_col and (r-c) not in row_m_col):
+
+                    cols.add(c)
+                    row_col.add(r + c)
+                    row_m_col.add((r - c))
+
+                    dfs(r + 1, c)
+
+                    cols.remove(c)
+                    row_col.remove(r + c)
+                    row_m_col.remove((r - c))
+
+            return 
+
+        
+        print(dfs(0, 0))
+
+        print(res)
+
+        return len(res)
