d4656229f3ceaf586bf33692a3c0abb4081782f6
michimomma-gmail-code <michimomma@gmail.com>
2026-06-26 11:51:51 -0700

    Add: evaluate-division - submission-2

---
+++ Data Structures & Algorithms/evaluate-division/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        n = len(equations)
+        adj = defaultdict(dict)
+
+        for i in range(n):
+            a, b = equations[i]
+            val = values[i]
+            adj[a][b] = val
+            adj[b][a] = 1 / val
+
+        def dfs(node, cur_val):
+            reachable = {node: cur_val }
+            seen.add(node)
+            for nxt_node, val in adj[node].items():
+                if (nxt_node not in seen) and (nxt_node != node):
+                    seen.add( nxt_node )
+                    temp = dfs(nxt_node, cur_val * val)
+                    reachable.update( temp )
+
+            return reachable
+
+        res = defaultdict(dict)
+        for node in adj:
+            seen = set()
+            res[node].update( dfs(node, 1) )
+
+        output = []
+        for a, b in queries:
+            if a in res and b in res[a]:
+                output.append(res[a][b])
+            else:
+                output.append(-1)
+
+        return output
