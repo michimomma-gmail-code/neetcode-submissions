316872fb3e7f4721585982be409155f05b30e407
michimomma-gmail-code <michimomma@gmail.com>
2026-07-01 15:46:59 -0700

    Add: minimum-height-trees - submission-1

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+    
+        adj = [ [] for _ in range(n) ]
+        degree = [0] * n
+        for u, v in edges:
+            degree[u] += 1
+            degree[v] += 1
+            adj[u].append(v)
+            adj[v].append(u)
+
+        queue = deque([i for (i, deg) in enumerate(degree) if deg <= 1])
+        print(degree)
+        seen = set()
+        print(queue)
+
+        while max(degree) > 1:
+
+            for _ in range(len(queue)):
+
+                node = queue.popleft()
+                degree[node] -= 1
+                seen.add(node)
+
+                for nei in adj[node]:
+                    if nei in seen:
+                        continue
+                    degree[nei] -= 1
+                    if degree[nei] == 1:
+                        queue.append(nei)
+
+        print(f'degree = {degree}, queue = {queue}')
+        return list(queue)                
