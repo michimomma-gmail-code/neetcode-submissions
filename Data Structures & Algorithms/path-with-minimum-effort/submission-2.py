496e390104a50b1d44f19f11c5c78bb2cee6f0ae
michimomma-gmail-code <michimomma@gmail.com>
2026-07-02 05:08:01 -0700

    Add: path-with-minimum-effort - submission-2

---
+++ Data Structures & Algorithms/path-with-minimum-effort/submission-2.py
@@ -0,0 +1,68 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        nrow = len(heights)
+        ncol = len(heights[0])
+
+        dt = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+#        minheap = [(heights[0][0], 0, 0)]
+        minheap = [(0, 0, 0)]
+
+        r, c = 0, 0
+        seen = set()
+        while minheap:
+#            print(minheap)
+
+            max_h, r, c = heapq.heappop(minheap)
+
+            if (r, c) in seen:
+                continue
+            seen.add( (r, c) )
+
+            h = heights[r][c]
+
+#            print(f'{r, c}, opt = {max_h}')
+
+            if r == nrow -1 and c == ncol - 1:
+#                print(seen)
+                return max_h
+
+            for dr, dc in dt:
+                nxt_r, nxt_c = r + dr, c + dc
+                if 0 <= nxt_r < nrow and 0 <= nxt_c < ncol and (nxt_r, nxt_c) not in seen:
+                    dh = abs( heights[nxt_r][nxt_c] - h )
+                    heapq.heappush(minheap, (max(max_h, dh), nxt_r, nxt_c ) )
+
+        return 
+
+
+        # starting from st: (r, c) to (r + dr, c + dc)
+        # distance = |heights[r + dr][c + dc] - heights[r][c]|
+        #
+        # [1, 1, 1]
+        # [3, 2, 4]
+        # [2, 5, 4]
+        #
+        # [[1], 1, 1] q = { (0, (0,0)) }
+        # [3, 2, 4]
+        # [2, 5, 4]
+        #
+        # [[1], [1], 1] q = { (2, (1, 0)), (0, (0, 1)) }
+        # [<3>, 2, 4]
+        # [2, 5, 4]
+        #
+        # [[1], [1], [1]] q = { (2, (1, 0)), (0, (0,2)), (1, (1,1)) }
+        # [<3>,  [2],  4]
+        # [2,    5,  4]
+        #
+        # [[1], [1], [1]] q = { (2, (1, 0)), (1, (1,1)) , (3, (1, 2))}
+        # [<3>,  [2],  [4]]
+        # [2,    5,  4]
+        #
+        # [[1], [1], [1]] q = { (2, (1, 0)), x(1, (1,1)) , (3, (1, 2)), (2, (2,1))}
+        # [<3>,  [2],  <4>]
+        # [2,    [5],  4]
+
+        # for each (r + dr, c + dc)
+        # compute min ( hights[r+dr][c+dc] - hights[r][c], )
+        # 
