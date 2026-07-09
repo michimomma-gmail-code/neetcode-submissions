f28770d4d690bb6eda8fad5d455856e3bcf060d8
michimomma-gmail-code <michimomma@gmail.com>
2026-07-09 16:38:23 -0700

    Add: last-stone-weight-ii - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        # min(abs(x + y), abs(x - y))
+        #
+        # dp: index - sum(stones)
+        # dp[i]: sum(stone) = i
+        #
+        # dp[0]: 0
+        # dp[1]: (2 - 1)
+        # dp[2]: 1+dp[1], 3-dp[1]
+        total = sum(stones)
+        target = total // 2
+
+        dp = [0] * (target + 1)
+#        dp[0] = 0
+#        num_s = len(stones)
+#        for j in range(num_s):
+        for stone in stones:
+            for i in range(len(dp) - 1, -1, -1):
+                if i - stone >= 0:
+                    dp[i] = max(dp[i], dp[i - stone] + stone)
+        
+        print(dp)
+        total_pos = dp[target]
+        total_neg = total - total_pos
+
+        return abs(total_pos - total_neg)
