97e54c95e45c723926cb4f5ed851db75c0d3e6b3
michimomma-gmail-code <michimomma@gmail.com>
2026-07-13 14:03:22 -0700

    Add: jump-game-vii - submission-29

---
+++ Data Structures & Algorithms/jump-game-vii/submission-29.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        if s[-1] == "1":
+            return False
+
+        n = len(s)
+        queue = deque([0])
+        # check and move for each q in queue
+        furthest = 0
+
+        while queue:
+            idx = queue.popleft()
+
+            st = max(idx + minJump, furthest)
+            ed = min(idx + maxJump, n - 1)
+
+            if st > n - 1:
+                return False
+
+            for i in range(st, ed + 1):
+                if s[i] == "0":
+                    if i == n - 1:
+                        return True
+                    queue.append(i)
+
+            furthest = max(furthest, ed + 1)
+    
+        return False
+                
+
