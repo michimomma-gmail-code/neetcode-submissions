0112c2d61cf662a165c935701e361d4838b6eecc
michimomma-gmail-code <michimomma@gmail.com>
2026-07-17 15:39:42 -0700

    Add: excel-sheet-column-title - submission-2

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-2.py
@@ -0,0 +1,51 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        # cn 
+        # 1 2   3  4    5   6    7   8  9   10 11 12
+        # A B  AA AB   BA  BB  AAA AAB ABA ABB A B
+
+        # 27^2 27^1 27^0
+        #
+
+
+        def dfs(cn):
+            if cn == 0:
+                return ""
+
+            num = cn - 1
+
+            res = dfs( num // 26) + chr(num % 26 + ord("A"))
+
+            return res
+
+        return dfs(columnNumber)
+
+
+
+    def convertToTitle(self, columnNumber: int) -> str:
+        # cn 
+        # 1 2   3  4    5   6    7   8  9   10 11 12
+        # A B  AA AB   BA  BB  AAA AAB ABA ABB A B
+
+        # 27^2 27^1 27^0
+        #
+        temp = []
+        def dfs(cn):
+            nonlocal temp
+            if cn == 0:
+                return 0
+
+            num = cn - 1
+            print('num = ', num)
+            print(chr(num % 26 + ord("A")))
+            temp.append(chr(num % 26 + ord("A")))
+            res = dfs( num // 26)
+#            print('res = ', res)
+            
+            return res
+
+        res = dfs(columnNumber)
+        print(temp)
+        temp.reverse()
+        return "".join(temp)
+
