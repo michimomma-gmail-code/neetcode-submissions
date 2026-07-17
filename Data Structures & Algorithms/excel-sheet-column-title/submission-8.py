ee0e8186fbd3adcfb1e43a5e63a916d081cf3355
michimomma-gmail-code <michimomma@gmail.com>
2026-07-17 16:00:21 -0700

    Add: excel-sheet-column-title - submission-8

---
+++ Data Structures & Algorithms/excel-sheet-column-title/submission-8.py
@@ -0,0 +1,65 @@
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        # cn 
+        # 1 2   3  4    5   6    7   8  9   10 11 12
+        # A B  AA AB   BA  BB  AAA AAB ABA ABB A B
+
+        # 27^2 27^1 27^0
+        #
+
+
+        def dfs(cn):
+            if cn == 0:
+                return ""
+
+            num = cn - 1
+
+            res = dfs( num // 26) + chr(num % 26 + ord("A"))
+
+            return res
+
+        return dfs(columnNumber)
+
+
+
+    def convertToTitle(self, columnNumber: int) -> str:
+        # cn 
+        # 1 2   3  4    5   6    7   8  9   10 11 12 
+        # A B  AA AB   BA  BB  AAA AAB ABA ABB A B
+
+        # 27^2 27^1 27^0
+        #
+        temp = ""
+        def dfs(cn):
+            nonlocal temp
+            if cn == 0:
+                return 0
+
+            num = cn - 1
+#            print('num = ', num)
+#            print(chr(num % 26 + ord("A")))
+#            temp.append(chr(num % 26 + ord("A")))
+            temp = chr(num % 26 + ord("A")) + temp
+            res = dfs( num // 26)
+#            print('res = ', res)           
+            return res
+
+        res = dfs(columnNumber)
+        print(temp)
+#        temp.reverse()
+        return temp
+
+
+    def convertToTitle(self, columnNumber: int) -> str:
+
+        num = columnNumber 
+        res = ""
+        while num > 0:
+            num -= 1
+            temp = chr( ( (num) % 26) + ord('A') )
+            print(temp)
+            res = temp + res 
+            num = num // 26
+
+
+        return res
