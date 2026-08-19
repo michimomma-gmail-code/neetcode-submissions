d37048bf6db4078a87af04ca2f501c1eb5e4542a
michimomma-gmail-code <michimomma@gmail.com>
2026-08-19 14:44:36 -0700

    Add: anagram-groups - submission-18

---
+++ Data Structures & Algorithms/anagram-groups/submission-18.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        n = len(strs)
+        res = defaultdict(list)
+        for s in strs:
+            array = [0] * 26 
+            for char in s:
+                idx = ord(char) - ord("a")
+                array[idx] += 1
+
+#            print(s, array)
+
+            #key = "_".join([ str(i) for i in array])
+            res[tuple(array)].append(s)
+
+#        print(res)
+
+        return list(res.values())
