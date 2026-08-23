9ce3fc5f13cf0dfda2ee3e516ca7e2ccc1b70bbe
michimomma-gmail-code <michimomma@gmail.com>
2026-08-23 07:53:42 -0700

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def isPalindrome(self, s: str) -> bool:
+        def alphanum(c):
+
+            return (ord("a") <= ord(c) <= ord("z") 
+            or ord("A") <= ord(c) <= ord("Z")
+            or ord("0") <= ord(c) <= ord("9"))
+
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if not alphanum(s[l]):
+                l += 1
+                continue
+            if not alphanum(s[r]):
+                r -= 1
+                continue
+#            print(s[l : r + 1])
+            if not s[l].lower() == s[r].lower():
+                return False
+            l += 1
+            r -= 1
+
+        return True
+        
