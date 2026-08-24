16a72cd00a5b8a639f461fcae39e1050aa9d2795
michimomma-gmail-code <michimomma@gmail.com>
2026-08-24 11:44:40 -0700

    Add: buy-and-sell-crypto - submission-14

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-14.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minval = prices[0]
+        prof = 0
+        for i in range(1, len(prices)):
+            prof = max(prof, prices[i] - minval)
+            minval = min(minval, prices[i])
+        
+        return prof
+
+    def maxProfit(self, prices: List[int]) -> int:
+        # state
+        # hold: maximum profit you can have if you end the day holding a stock
+        # sold: maximum profit you can have if you end the day not holding a stock
+        # state tarnsition
+        # hold -> hold 
+        # hold -> sold
+        # sold -> hold
+        # sold -> sold
+        hold = - float("inf")
+        sold = 0
+        for p in prices:
+            hold = max(hold, -p)
+            sold = max(sold, hold + p)
+
+        return sold
