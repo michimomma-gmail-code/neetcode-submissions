7de9ad0eaed07565503af4e295182d32ad374958
michimomma-gmail-code <michimomma@gmail.com>
2026-08-24 13:16:07 -0700

    Add: longest-substring-without-duplicates - submission-24

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-24.py
@@ -0,0 +1,197 @@
+class Solution:
+    def lengthOfLongestSubstring0(self, s: str) -> int:
+        # start from i, probe with j
+        # store seen for the char checked s[j]
+        # if s[j] in seen, remove s[j] from seen. probe with j end
+        # else, add s[j] in seen, proceed (j += 1)
+
+        max_length = 0
+
+        for i in range(len(s)):
+            seen = set()
+            seen.add(s[i])
+            length = 1
+            j = i + 1
+            while j < len(s):
+                if s[j] in seen:
+                    break
+                else:
+                    length += 1
+                    seen.add(s[j])
+                    j += 1
+            if length > max_length:
+                max_length = length
+
+        return max_length
+
+    def lengthOfLongestSubstring0(self, s: str) -> int:
+        seen = set()
+
+        l = 0
+        max_length = 0
+        for r in range(len(s)):
+            char = s[r]
+            while char in seen:
+                seen.remove(s[l])
+                l += 1
+            length = r - l + 1
+            max_length = max(max_length, length)
+            seen.add(s[r])
+        return max_length
+
+
+    def lengthOfLongestSubstring1(self, s: str) -> int:
+        seen = set()
+        left = 0
+        max_length = 0
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+            length = right - left + 1
+            max_length = max(max_length, length)
+            seen.add(s[right])
+        return max_length
+
+        # last_seen = {}
+        # left = 0
+        # max_length = 0
+
+        # for right, char in enumerate(s):
+        #     if char in last_seen and last_seen[char] >= left:
+        #         left = last_seen[char] + 1
+        #     last_seen[char] = right
+        #     max_length = max(max_length, right - left + 1)
+
+        # return max_length
+
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_length = 0
+        left = 0
+        seen = set()
+        n = len(s)
+
+        for right in range(n):
+            char = s[right]
+#            print(f'char = {char}, seen = {seen}')
+            while char in seen:
+                seen.remove(s[left])
+                left += 1
+
+            if char not in seen:
+                seen.add(char)
+
+            max_length = max(max_length, right - left + 1)
+        
+        return max_length
+
+
+    def lengthOfLongestSubstring1(self, s: str) -> int:
+        max_length = 0
+        left = 0
+        last_seen = {}
+        n = len(s)
+
+        for right in range(n):
+            char = s[right]
+
+            if char in last_seen:
+                left = max(left, last_seen[char] + 1)
+
+            last_seen[char] = right
+
+            length = right - left + 1
+            max_length = max(max_length, length)
+
+        return max_length
+
+
+
+
+
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = set()
+        l = 0
+        maxlen = 0
+        for r in range(len(s)):
+            if s[r] not in seen:
+                maxlen = max(maxlen, r - l + 1)
+            else:
+#                print(f"seen = {seen}, r = {r}, s[r] = {s[r]}")
+                while l < r and s[r] in seen:
+#                    print("s[r] = ", s[r], "s[l] = ", s[l])
+                    seen.remove(s[l])
+                    l += 1
+#            print(f'r = {r}, l = {l}')
+            seen.add(s[r])
+        
+        return maxlen
+                
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # index of last seen
+        last_seen = {} # e.g., last_seen["a"]: 1
+        # this needs to be updated once there is a dup
+        # and left pointer is also updated to the previou
+
+        max_len = 0
+        l = 0
+        for r in range(len(s)):
+            if s[r] not in last_seen or last_seen[s[r]] < l:
+                max_len = max(max_len, r - l + 1)
+            else:
+                l = last_seen[s[r]] + 1
+
+            last_seen[s[r]] = r
+
+        return max_len
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
