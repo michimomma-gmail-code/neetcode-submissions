b45fbf8ecb109f7f4e16d6fc4fa482dba167e7d5
michimomma-gmail-code <michimomma@gmail.com>
2026-08-31 10:43:12 -0700

    Add: valid-binary-search-tree - submission-18

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-18.py
@@ -0,0 +1,113 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        def dfs(root, lb, ub):
+            if not root:
+                return True
+
+            if not (lb < root.val < ub):
+                return False
+
+            left = dfs(root.left, lb, root.val)
+            right = dfs(root.right, root.val, ub)
+    
+            if left and right:
+                return True
+            else:
+                return False
+
+        res = dfs(root, -float('infinity'), float('infinity'))
+
+        return res
+
+
+
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+
+        def dfs(node, lb, ub):
+
+            if not node:
+                return True
+
+            print(f'lb = {lb}, ub = {ub}, val = {node.val}')
+
+            if node.val <= lb or node.val >= ub:
+                return False
+
+            if not dfs(node.left, lb, min(ub, node.val)):
+                return False
+            if not dfs(node.right, max(lb, node.val), ub):
+                return False
+
+            return True
+
+
+        return dfs(root, -float("infinity"), float("infinity"))
+
+
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # largest from left sub tree < node
+        # node < smallest from right sub tree
+
+        def dfs(node, lb, ub):
+            if not node:
+                return True
+            
+            if node.val <= lb:
+                print(node.val, lb)
+                return False
+            if node.val >= ub:
+                print(node.val, ub)
+                return False
+
+            if not dfs(node.left, lb, node.val):
+                return False
+            if not dfs(node.right, node.val, ub):
+                return False
+
+            return True
+
+        return dfs(root, -float("inf"), float("inf"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
