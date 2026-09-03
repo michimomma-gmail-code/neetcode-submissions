a1804c50e16ae4e128a240871543350d727486a4
michimomma-gmail-code <michimomma@gmail.com>
2026-09-03 16:50:14 -0700

    Add: binary-tree-inorder-traversal - submission-3

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-3.py
@@ -0,0 +1,106 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal0(self, root: Optional[TreeNode]) -> List[int]:
+        
+        res = []
+
+        def dfs(node):
+            if not node:
+                return
+
+            dfs(node.left)
+            res.append(node.val)
+            dfs(node.right)
+
+            return
+
+        dfs(root)
+
+        return res
+
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        cur = root
+
+        while cur or stack:
+
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+
+        return res
+
+
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        cur = root
+        stack = []
+
+        res = []
+
+        while cur or stack:
+
+            # left until end
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+
+            # exhausted, push to res
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+            
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
