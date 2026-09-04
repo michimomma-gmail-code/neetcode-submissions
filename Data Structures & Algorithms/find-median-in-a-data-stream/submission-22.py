c9915c1b718c1b7abb77c26338c9f9378457c279
michimomma-gmail-code <michimomma@gmail.com>
2026-09-04 15:51:18 -0700

    Add: find-median-in-a-data-stream - submission-22

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-22.py
@@ -0,0 +1,123 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.lowerhalf = [] #max heap
+        self.upperhalf = [] #min heap
+        self.count = 0
+
+    def addNum(self, num: int) -> None:
+
+        prev_lh = None
+        prev_uh = None
+        if len(self.lowerhalf) > 0:
+            prev_lh = self.lowerhalf[0]
+        if len(self.upperhalf) > 0:        
+            prev_uh = self.upperhalf[0]
+
+        heapq.heappush(self.lowerhalf, -num)
+        heapq.heappush(self.upperhalf, num)
+
+        if prev_lh:
+            heapq.heappush(self.lowerhalf, -prev_uh)
+        if prev_uh:
+            heapq.heappush(self.upperhalf, -prev_lh)
+
+        self.count += 1
+        if self.count % 2 == 0: # even
+            h = self.count / 2
+        else:
+            h = self.count // 2 + 1
+
+#        print(f'h = {h}')
+        while len(self.lowerhalf) > h:
+            heapq.heappop(self.lowerhalf)
+#            print(f'remove {heapq.heappop(self.lowerhalf)}')
+        while len(self.upperhalf) > h:
+            heapq.heappop(self.upperhalf)
+
+
+    def findMedian(self) -> float:
+#        print(f'lh = {-self.lowerhalf[0]}, hh = {self.upperhalf[0]}')
+        return (-self.lowerhalf[0] + self.upperhalf[0]) / 2
+
+
+
+
+
+
+
+    def __init__(self):
+        self.n = 0
+        self.max_heap = [] # for top n//2
+        self.min_heap = [] # for low n//2
+
+    def addNum(self, num: int) -> None:
+        self.n += 1
+
+#        capacity = (self.n + 1) // 2
+        # n = 1 -> [1] [1]
+        # n = 2 -> [1] [2]
+        # n = 3 -> [1 <3>] [<2> 3] : [1 2] [2 3]
+        # n = 4 -> [1 <3>] [<3> 4] : [1 2] [<3> 4]
+        # n = 5 -> [1 2 3] [3 4 5]
+        # n = 6 -> [1 2 3] [4 5 6]
+        # n = 7 -> [1 2 3 4 = min(7,4)] [4 5 6 7 = max(3, 7)]
+        # n = 8 -> [1 2 3 4 min(8,4)] [4 5 6 7 max(4, 8)]
+
+        # top half
+        heapq.heappush(self.max_heap, - num)
+
+        # boundary
+        if self.max_heap and self.min_heap and (-self.max_heap[0]) > self.min_heap[0]:
+            val = - heapq.heappop(self.max_heap)
+            heapq.heappush(self.min_heap, val)
+
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            val = - heapq.heappop(self.max_heap)
+            heapq.heappush(self.min_heap, val)
+        elif len(self.max_heap) < len(self.min_heap):
+            val = heapq.heappop(self.min_heap)
+            heapq.heappush(self.max_heap, -val)
+
+
+
+    def findMedian(self) -> float:
+
+        if self.n % 2 == 0:
+            return (self.min_heap[0] - self.max_heap[0]) / 2.0
+        else:
+            return - self.max_heap[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
