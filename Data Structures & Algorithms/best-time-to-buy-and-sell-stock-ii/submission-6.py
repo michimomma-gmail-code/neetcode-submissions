23483e49c63d6c293df868cb07edbc396c8ddda9
michimomma-gmail-code <michimomma@gmail.com>
2026-09-05 11:04:33 -0700

    Add: best-time-to-buy-and-sell-stock-ii - submission-6

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-6.py
@@ -0,0 +1,111 @@
+class Solution:
+    def maxProfit0(self, prices: List[int]) -> int:
+        # profit: 
+        # at i, buy (price i).
+        #  or sell (price i - price j)
+        #  or hold
+        # dp[i] = max( price[i] - price[j], dp[i - 1] )
+        #
+        # [1, 2, 3, 4, 5]
+        # dp[0] = 0
+        # dp[1] = max( dp[j] + price[1] - price[0], dp[0])
+        # dp[2] = max( dp[j] + price[2] - price[j], dp[1])
+        n = len(prices) 
+        dp = [0] * n
+
+        for i in range(1, n):
+
+            for j in range(i):
+                dp[i] = max(dp[i], dp[j] + prices[i] - prices[j], dp[i - 1])
+
+        print(dp)
+
+        return max(dp)
+
+    def maxProfit2(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        if len(prices) == 1:
+            return 0
+
+        profit = 0
+        for i in range(1, len(prices)):
+            profit += max(prices[i] - prices[i - 1], 0)
+
+        return profit
+
+    def maxProfit(self, prices: List[int]) -> int:
+# free: have no stock 
+# hold: hold a stock
+        free = 0
+        hold = - prices[0]
+
+        for i in range(1, len(prices)):
+
+            free = max(free, hold + prices[i])
+            hold = max(hold, free - prices[i])
+
+        print(free, hold)
+        
+        return free
+
+
+    def maxProfit(self, prices: List[int]) -> int:
+        # state (end of day)
+        # hold (has one stock)
+        # free (no stock)
+        # transition
+        #
+        # hold -> hold (did nothing)
+        # free -> hold (bought -neg profit)
+        #
+        # hold -> free (sold)
+        # free -> free (did nothing) 
+
+        hold = - float("inf")
+        free = 0
+        for p in prices:
+            hold = max( hold, free -p )
+            free = max( free, p + hold)
+            print(p, hold, free)
+
+        return free
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
