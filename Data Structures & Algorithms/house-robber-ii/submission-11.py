9f6e5958ab1f83eba6ad217a4b111e06edef97c7
michimomma-gmail-code <michimomma@gmail.com>
2026-09-07 12:14:29 -0700

    Add: house-robber-ii - submission-11

---
+++ Data Structures & Algorithms/house-robber-ii/submission-11.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        if len(nums) == 1:
+            return nums[0]
+
+
+        def runDP(st, ed):
+            rob = - 1
+            nothing = 0
+            for i in range(st, ed):
+                rob, nothing = nothing + nums[i], max(nothing, rob)
+
+#            print(rob, nothing)
+            return max(rob, nothing)
+
+        # s1: can take 1st, not take last
+        s1 = runDP(0, len(nums) - 1)
+        # s2: cannot take 1st, can take last
+        s2 = runDP(1, len(nums))
+
+        return max(s1, s2)
